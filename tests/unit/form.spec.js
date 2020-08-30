f6bd369bf47849b5f2252cfc8ef6c7d186be95a3
Daniel-Fang <f455039690@163.com>
2020-08-31 00:40:17 +0800

    添加form test

---
+++ tests/unit/form.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils';
+import FyForm from '@/components/form';
+
+describe('test form', () => {
+  it('test form', () => {
+    const wrapper = shallowMount(FyForm);
+  })
+})
